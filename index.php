97422ef87440907c80b58298f301e0031f03a96f
m0k1 <moki.forum@gmail.com>
2013-12-31 05:35:21 +0100

    Fixed file pagination + config.php added.

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-	$get = $_GET['x'];
-    if(strpos($_GET['x'],'/language/') !== false) {
-		$loadurl = file_get_contents("c");
-		echo($loadurl);
-		exit;
-	}
-	if($get == "/settings" || $get == "/language" ||  $get == "/" || $get == "")
-	{
-		$loadurl = file_get_contents("c");
-		echo($loadurl);
-		exit;
-	}
-	include("includes/functions.php");
-	include("includes/dn.php");
-    $loadurl = $get;
-    $loadurl = "http://$domaintoproxy$loadurl";
-    $loadurl = str_replace(" ", "+", $loadurl);
-    $loadurl = get_data("$loadurl");
-	$loadurl = remove_bloat("$loadurl");
-	echo($loadurl);
-	exit;
+$get = $_GET['x'];
+if (strpos($_GET['x'], '/language/') !== false)
+{
+    $loadurl = file_get_contents("c");
+    echo ($loadurl);
+    exit;
+}
+if ($get == "/settings" || $get == "/language" || $get == "/" || $get == "")
+{
+    $loadurl = file_get_contents("c");
+    echo ($loadurl);
+    exit;
+}
+include("includes/functions.php");
+include("includes/dn.php");
+$loadurl = $get;
+$loadurl = "http://$domaintoproxy$loadurl";
+$loadurl = str_replace(" ", "+", $loadurl);
+$loadurl = get_data("$loadurl");
+$loadurl = remove_bloat("$loadurl");
+echo ($loadurl);
+exit;
 ?>
