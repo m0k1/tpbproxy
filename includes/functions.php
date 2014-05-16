8c41614cefb72cd98d046523491478b2af00ece4
m0k1 <moki.forum@gmail.com>
2014-05-17 01:56:43 +0200

    Small fixes ...

--- includes/functions.php
+++ includes/functions.php
@@ -122,15 +122,15 @@ function get_data($url)
 function search_curl($url)
 {
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'); // Only calling the head
-	curl_setopt($ch, CURLOPT_HEADER, true); // header will be at output
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD'); // HTTP request is 'HEAD'	
-	$data = curl_exec($ch);
-	curl_close($ch);
-	return $data;
+ 	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+ 	curl_setopt ($ch, CURLOPT_URL, $url);
+	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 20);
+ 	curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11');// Only calling the head
+ 	curl_setopt($ch, CURLOPT_HEADER, true); // header will be at output
+ 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD'); // HTTP request is 'HEAD'	
+ 	$data = curl_exec($ch);	
+	curl_close($ch);	
+ 	return $data;
 }
 
 ?>
